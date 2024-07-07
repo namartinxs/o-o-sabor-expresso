178ea9d877d07735fab479fd15bde0db095db6c4
Nathalia Martins <nathaliafloresmartins4550@gmail.com>
2024-07-07 19:15:37 -0300

    Aula 2

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,20 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self,nome,categoria):    
+        self.nome = nome
+        self.categoria = categoria 
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_pizaa = Restaurante()
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-restaurantes =[restaurante_pizaa,restaurante_praca]
-print(dir(restaurante_praca))
+restaurante_praca = Restaurante('Praça','Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+
+Restaurante.listar_restaurantes()
